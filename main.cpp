b39876075300d5763fbea956c6555651fb25301b
xuelinf <snowfacewind@gmail.com>
2015-12-03 14:23:56 +0800

    Cmake file, stl 特殊方法，函数指针方式。

--- main.cpp
+++ main.cpp
@@ -3,16 +3,33 @@
 //
 
 #include <iostream>
-#include <string>
-#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
+int fcmp(const void* elem1, const void* elem2){
+    const int* i1 = (const int*)elem1;
+    const int* i2 = (const int*)elem2;
+
+    if (*i1 < *i2){
+        return -1;
+    } else if(*i1 == *i2){
+        return 0;
+    } else if( *i1 > *i2){
+        return 1;
+    }
+}
+
 int main(){
-    vector<int>tmp = {1,2,3,4,5};
-    for(auto &i : tmp){
-        cout << i <<endl;
-        cout << "test" <<endl;
+    int ia[10] = {32,92,67,58,10,4,25,52,59,54};
+    for(int i = 0; i < 10; i++){
+        cout << ia[i] << " ";
+    }
+
+    qsort(ia, sizeof(ia)/sizeof(int), sizeof(int), fcmp);
+
+    for(int i = 0;i < 10; i++){
+        cout << ia[i] << " ";
     }
     return 0;
 }
